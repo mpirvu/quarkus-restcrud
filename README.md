7315025fe7b823adbb0d2c56c9ab8a297f59ceeb
Marius Pirvu <mpirvu@ca.ibm.com>
2026-01-21 14:05:53 -0800

    Push sources for restcrud app
    Signed-off-by: Marius Pirvu <mpirvu@ca.ibm.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # quarkus-restcrud
 Repo for building/running a simple restCrud app on top of Quarkus with OpenJ9 and InstantON
+Based on https://github.com/jdmcclur/restCrud
+
---
+++ src/main/java/com/acme/crud/FruitApplication.java
@@ -0,0 +1,7 @@
+package com.acme.crud;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class FruitApplication extends Application {}
